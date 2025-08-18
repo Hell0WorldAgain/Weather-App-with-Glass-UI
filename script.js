9088b59f9029ec1b53251f7415662c001d52b0bd
Abhishek Choudhary <corporateabhishek47@gmail.com>
2025-08-19 03:11:54 +0530

    Designed a Weather App using Glass UI

---
+++ script.js
@@ -0,0 +1,42 @@
+const button = document.getElementById("search_button");
+const input = document.getElementById("city_input");
+
+const cityName = document.getElementById('city_name');
+const cityTemp = document.getElementById('city_temp');
+const cityTime = document.getElementById('city_time');
+const tempFeels = document.getElementById('feels');
+const humidity = document.getElementById('humidity');
+const wind = document.getElementById('wind');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+
+async function getData(cityName){
+    const promise = await fetch(
+        `http://api.weatherstack.com/current?access_key=b19de85fa2d6d64ed18d0d5c40e79821&query=${cityName}`
+    );
+    return await promise.json();
+}
+
+button.addEventListener('click', async () => {
+    //console.log(input.value);
+    // alert('Clicked');
+
+    const value = input.value;
+    const result = await getData(value);
+
+    console.log(result);
+
+    cityName.innerText = `${result.location.name}, ${result.location.region}, ${result.location.country}`;
+    cityTime.innerText = `${result.location.localtime}`;
+    cityTemp.innerText = `${result.current.temperature}°C`;
+    tempFeels.innerText = `${result.current.feelslike}°C`;
+    humidity.innerText = `${result.current.humidity}%`;
+    wind.innerText = `${result.current.wind_speed} km/h`;
+    latitude.innerText = `${result.location.lat}°`;
+    longitude.innerText = `${result.location.lon}°`;
+    sunrise.innerText = `${result.current.astro.sunrise}`;
+    sunset.innerText = `${result.current.astro.sunset}`;
+
+})
